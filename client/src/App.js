6e6f0f594a82bfccc51cea9c097b771f8117dfd1
Owen Li <owen.li@mail.mcgill.ca>
2018-11-09 19:31:28 -0500

    fix router

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import {BrowserRouter as Router, Link, Switch, Route} from 'react-router-dom';
-import Home from '/client/src/Home';
+import Home from './Home';
 
 class App extends Component {
   state = {users: []}
 
-  componentDidMount() {
-    fetch('/users')
-      .then(res => res.json())
-      .then(users => this.setState({ users }));
-  }
+  // componentDidMount() {
+  //   fetch('/users')
+  //     .then(res => res.json())
+  //     .then(users => this.setState({ users }));
+  // }
 
   render() {
     return (
@@ -21,14 +21,8 @@ class App extends Component {
               <Link to="/">Home</Link>
             </li>
           </ul>
+          <Route exact path="/" component={ Home }/>
         </div>
-        <div className="App">
-          <h1>Users</h1>
-          {this.state.users.map(user =>
-            <div key={user.id}>{user.username}</div>
-          )}
-        </div>
-        <Route path="/" exact component={Home}/>
       </Router>
     );
   }
