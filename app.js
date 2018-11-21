10b78bf7d8d201e8d676181f4f4b462c942d805e
jtdev <j.jthyriar@gmail.com>
2018-11-20 21:22:53 -0500

    DB has been setup, registration endpoint setup.

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var usersRouter = require('./routes/users');
 var profileRouter = require('./routes/profile');
 var postingsRouter = require('./routes/postings');
+require('dotenv').config();
+var expressValidator = require('express-validator');
 
 var app = express();
 
@@ -15,8 +18,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
