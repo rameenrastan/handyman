fc08341c9448b6d9407991205ef9a50d8a944e33
jtdev <j.jthyriar@gmail.com>
2018-11-28 15:25:05 -0500

    Login endpoint created. JWT is assigned to user upon login. Need to implement passport to use token.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 // Load User Model
 const User = require('../../models/User');
+
+// Load Secret key
+const key = require('../../config/keys').secretOrKey;
 
 // This is just a test route...
 router.get('/test', (req,res) => {
@@ -28,6 +32,35 @@ router.post('/register', (req,res) => {
             .catch(err => console.log(err));
         });
       }
+    })
+    .catch(err => res.status(400).json({ error: err }));
+});
+
+
+//@route POST api/users/login
+//@desc Login user
+//@access Public
+router.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  User.findOne({ email })
+    .then(user => {
+      // if user does not exist
+      if(!user) {
+        res.status(400).json({ error: 'Email or password is incorrect. Plesae try logging in again.'});
+      } 
+      // Check password
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if(isMatch) {
+            // create JWT if matched
+            console.log(user);
+            const payload = { id: user._id, name: user.name }; // payload
+            jwt.sign(payload, key, { expiresIn: 3600 }, (error, token) => { res.json({ success: true, token: 'Bearer ' + token}); 
+            });
+          } else {
+            res.status(400).json({ error: 'Email or password is incorrect. Please try logging in again.'})
+          }
+        });
     })
     .catch(err => res.status(400).json({ error: err }));
 });
