4ae0ffca0583a741845b03a24991cbde6776d9ea
jtdev <j.jthyriar@gmail.com>
2018-11-27 20:17:16 -0500

    routes added, restructered app.js file to server.js with what we need.

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+// Route handlers
+const users = require('./routes/api/users');
+const profiles = require('./routes/api/profiles');
+const services = require('./routes/api/services');
+
+const app = express();
+
+// Body-parser middlware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // DB config
 const db = require('./config/keys').mongoURI;
@@ -9,11 +21,10 @@ mongoose.connect(db, { useNewUrlParser: true })
   .then(() => console.log('MongoDB connected.'))
   .catch(error => console.log(error));
 
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send("This works!");
-})
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profiles', profiles);
+app.use('/api/services', services);
 
 const port = process.env.PORT || 5000;
 
